8e6d0d50de6240140824b0fe534a47e4c905176b
Daali <daalivardhan55@gmail.com>
2023-02-16 21:50:05 +0530

    cors check

--- server/server.js
+++ server/server.js
@@ -8,7 +8,9 @@ dotenv.config()
 
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin:"https://daalivardhan.github.io/ImageGeneratorOpenAI"
+}))
 app.options('*', cors());
 app.use(express.json({limit:"50mb"}))
 app.use("/getImg",postRoute)
