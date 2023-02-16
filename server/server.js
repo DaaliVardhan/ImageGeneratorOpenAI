8c09d72dc3d3ef94713eb37ea4b987516ecf7c8e
Daali <daalivardhan55@gmail.com>
2023-02-16 20:14:12 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,9 @@ dotenv.config()
 
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: 'https://daalivardhan.github.io/ImageGeneratorOpenAI'
+}))
 app.use(express.json({limit:"50mb"}))
 app.use("/getImg",postRoute)
 app.use("/save",saveRoute)
