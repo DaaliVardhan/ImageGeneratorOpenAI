f1fd1e1637c2fba3fc8ffa8cc9f6ba7983e5a5cb
Daali <daalivardhan55@gmail.com>
2023-02-16 21:44:33 +0530

    cors check

--- server/server.js
+++ server/server.js
@@ -8,9 +8,7 @@ dotenv.config()
 
 
 const app = express()
-app.use(cors({
-    origin: 'https://daalivardhan.github.io'
-}))
+app.use(cors())
 app.options('*', cors());
 app.use(express.json({limit:"50mb"}))
 app.use("/getImg",postRoute)
